5901fed005a4412419ec36bdada10f8312013c61
liwenbiao <1758123337@qq.com>
2024-11-02 14:57:40 +0800

    测试 优化器学习率调度器包裹类

--- tests/optim/test_optim_scheduler_wrapper.py
+++ tests/optim/test_optim_scheduler_wrapper.py
@@ -43,30 +43,11 @@ class TestOptimSchedulerWrapper(unittest.TestCase):
         for data, label in zip(torch.randn(100, 1), torch.randn(100, 1)):
             loss = loss_fct(model(data), label)
 
-            """
-            PyTorch模型训练核心步骤:
-                1. 梯度清零: optimizer.zero_grad()
-                2. 反向传播: loss.backward()
-                3. 参数更新: optimizer.step()
-            
-            它们之间的顺序要求:
-                1. 梯度清零 只能写在最前面或最后面
-                2. 反向传播 要写在 参数更新 之前
-            
-            所以有以下两种写法:
-            (1) optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-
-            (2) loss.backward()
-                optimizer.step()
-                optimizer.zero_grad()
-            """
-
-            optimizer.zero_grad()  # 梯度清零
             loss.backward()  # 反向传播求解梯度
             nn.utils.clip_grad_norm_(model.parameters(), 1.0)  # 梯度裁剪
             optimizer.step()  # 更新权重参数
+            optimizer.zero_grad()  # 梯度清零
+
             scheduler.step()  # 更新学习率
 
             # print(scheduler.get_lr(), {name: param for name, param in model.named_parameters()})
