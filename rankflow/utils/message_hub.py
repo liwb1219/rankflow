d564a1c855bd217efd85473a29c1f5b6cd4dca3d
liwenbiao <1758123337@qq.com>
2024-12-22 23:06:10 +0800

    更新 MessageHub

--- rankflow/utils/message_hub.py
+++ rankflow/utils/message_hub.py
@@ -1,7 +1,28 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2024 liwenbiao. All rights reserved.
 
+from typing import Any, Optional
 
 class MessageHub:
+    """
+    消息中心类, 用于存储和读取信息
+
+    功能:
+    - 存储或更新信息
+    - 获取信息, 支持指定默认值
+    """
     def __init__(self):
-        pass
+        """ 初始化消息中心, 创建一个空字典 """
+        self._storage = {}
+
+    def update_info(self, key: str, value: Any) -> None:
+        self._storage[key] = value
+
+    def get_info(self, key: str, default: Optional[Any] = None) -> Any:
+        return self._storage.get(key, default)
+
+    def get_info(self, key: str, default: Optional[Any] = None) -> Any:
+        return self._storage.get(key, default)
+
+    def get_info(self, key: str, default: Optional[Any] = None) -> Any:
+        return self._storage.get(key, default)
