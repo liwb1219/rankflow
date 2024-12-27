b69e900a97ec9fd8b2920b1fdfa5f7cc87d155f2
liwenbiao <1758123337@qq.com>
2024-12-27 22:18:17 +0800

    更新 CheckpointHook

--- rankflow/engine/hooks/checkpoint_hook.py
+++ rankflow/engine/hooks/checkpoint_hook.py
@@ -7,3 +7,11 @@ from .priority import HookPriority
 
 class CheckpointHook(HookBase):
     priority = HookPriority.LOWEST
+
+    def after_iter(self):
+        if self.trainer.save_mode == 'step' and self.trainer.step % self.trainer.save_interval == 0:
+            self.trainer.save_checkpoint()
+
+    def after_epoch(self):
+        if self.trainer.save_mode == 'epoch' and self.trainer.epoch % self.trainer.save_interval == 0:
+            self.trainer.save_checkpoint(model_dir=f'checkpoint-{self.trainer.epoch}')
