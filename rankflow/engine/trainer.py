f8ef1720ef9efa2cf1b5d35e8def13798355cb4a
liwenbiao <1758123337@qq.com>
2024-12-29 19:17:06 +0800

    更新 Trainer

--- rankflow/engine/trainer.py
+++ rankflow/engine/trainer.py
@@ -410,6 +410,7 @@ class Trainer:
                 batch_size=batch_size,
                 num_workers=num_workers,
                 pin_memory=pin_memory,
+                drop_last=True,
             )
         else:
             raise RuntimeError
