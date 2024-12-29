97cb62ae96e4aa45a19648e7d0a7d2520173be97
liwenbiao <1758123337@qq.com>
2024-12-29 13:29:09 +0800

    更新 Trainer

--- rankflow/reader/__init__.py
+++ rankflow/reader/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2024 liwenbiao. All rights reserved.
+
+from .data_reader import MapDataReader, IterableDataReader
+
+__all__ = [
+    'MapDataReader',
+    'IterableDataReader',
+]
